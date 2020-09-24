8f5499a4a08210c4124e3adfd42bd5e8bd568245
Wellington Lourenço Borges <53665329+wlborges@users.noreply.github.com>
2020-09-24 20:34:46 -0300

    v1

---
+++ src/pages/Home/index.js
@@ -0,0 +1,69 @@
+import React,{useState} from 'react';
+import '../../global.css';
+import './styles.css'
+
+export default function Home(){
+    const [local1,setLocal1]=useState([])
+    const [local2,setLocal2]=useState([])
+    const [local3,setLocal3]=useState([])
+    const [local4,setLocal4]=useState([])
+    const [insere,setInserir] = useState("")
+    const [remove,setRemover] = useState("")
+    const [locais, setLocais] = useState([]) //insere a quantidade em cada local
+    const [result,setResult]=useState('')
+
+    function contLocais(){
+        locais.push(local1.length)
+        locais.push(local2.length)
+        locais.push(local3.length)
+        locais.push(local4.length)
+    }
+
+    function inserir(e){
+
+
+        e.preventDefault()
+        local1.push(insere)
+        setResult('inserido '+ insere)
+
+
+    }
+
+    function remover(e){
+        e.preventDefault()
+        local1.pop()
+
+    }
+    
+    return(
+        <div className="geral">
+            <h1>Prova</h1>
+            <form onSubmit={inserir}>
+                <h3>Digite o código para inserir o container</h3>
+                <input
+                    placeholder="Inserir"
+                    value={insere}
+                    onChange={e=> setInserir(e.target.value)}
+                />
+                <button type="submit">Inserir</button>
+            </form>
+            <form onSubmit={remover}>
+                <h3>Remover container</h3>
+                <input
+                    placeholder="Remover"
+                    value={remove}
+                    onChange={e=> setRemover(e.target.value)}
+                />
+                <button type="submit">Remover</button>
+            </form>
+            <h2> {result} </h2>
+            <br/>
+            <h3>
+                Local 1 : (base) {local1} (topo) <br/>
+                Local 2 : (base) {local2} (topo) <br/>
+                Local 3 : (base) {local3} (topo) <br/>
+                Local 4 : (base) {local4} (topo) <br/>
+            </h3>
+        </div>
+    );
+}
